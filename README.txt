ee91c0905896605e08943048c74fa30a80635866
aymitator_cli <hollawac@outlook.com>
2025-11-08 16:36:06 -0800

    bigbigchanges

--- README.txt
+++
@@ -1,6 +0,0 @@
-sudo apt install git
-cd ~/Downloads
-git clone https://github.com/aymitator/todo
-cd todo #enjoy
-#
-emite is my best mini editor
--- diablo-2-tools/share/diablo-2-tools/make free.pl
+++
@@ -1,8 +0,0 @@
-use warnings;
-use strict;
-use feature 'say';
-print "'\@37make00001111' ";
-for my $i (643 .. 647, 667 .. 671, 691 .. 695){
-  print "'\@${i}make11111111' ";
-}
-say '';
